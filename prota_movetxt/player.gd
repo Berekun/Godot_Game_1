d559e5a94bd3ec8a59f2bf61b608296b5aff912f
Berekun <xnatsuhd@gmail.com>
2023-09-27 14:25:07 +0200

    27/09/23

--- prota_movetxt/player.gd
+++ prota_movetxt/player.gd
@@ -5,6 +5,10 @@ const max_speed = 150
 const friction = 1000
 
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 	
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else: 
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
-	move_and_collide(velocity * delta)
+	velocity = move_and_slide(velocity)
