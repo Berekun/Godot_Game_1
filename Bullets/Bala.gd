79e9279d61aea57e2c7ed4411184492f1304e247
Berekun <xnatsuhd@gmail.com>
2023-09-29 22:59:49 +0200

    bullets

---
+++ Bullets/Bala.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var movement = Vector2.ZERO
+
+var velocity  = 300 
+var angle = 0
+
+func _ready():
+	movement.x = cos(angle)
+	movement.y = sin(angle)
+	
+func _physics_process(delta):
+	
+	movement = movement.normalized() * velocity
+	movement = move_and_slide(movement)
+	
+func _on_Area2D_body_entered(body):
+	self.queue_free()
--- prota_movetxt/player.gd
+++ prota_movetxt/player.gd
@@ -4,16 +4,22 @@ const acceleration = 1000
 const max_speed = 150
 const friction = 1000
 
+const Bala = preload("res://Bullets/Bala.tscn")
+
+
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
 	
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = 	Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = 	Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
@@ -26,3 +32,16 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
 	velocity = move_and_slide(velocity)
+
+func shoot():
+	if Input.is_action_pressed("shoot"):
+		var bala = Bala.instance()
+		bala.position = self.position
+		bala.angle = get_angle_to(get_global_mouse_position())
+		get_parent().add_child(bala)
+		
+
+
+
+func _on_Timer_timeout():
+	shoot()
