06522bca20c55fc6858a82b47c7ad371b2f39a71
Berekun <xnatsuhd@gmail.com>
2023-10-03 14:21:45 +0200

    03/10/23

--- enemy_movetxt/DarkWizzard.gd
+++ enemy_movetxt/DarkWizzard.gd
@@ -7,7 +7,7 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-var movement = Vector2.ZERO
+onready var movement = player.position - self.position
 
 var velocity = 100
 
@@ -15,16 +15,14 @@ func _ready():
 	$Timer.start(1)
 
 func _physics_process(delta): 
-	movement = player.position - self.position
-	
-	if movement != Vector2.ZERO:
+	if movement == Vector2.ZERO:
+		animationState.travel("Idle")
+	else:
+		movement = player.position - self.position
 		animationTree.set("parameters/Idle/blend_position", movement)
 		animationTree.set("parameters/run/blend_position", movement)
 		animationState.travel("run")
 		movement = movement.normalized() * velocity
-	else: 
-		animationState.travel("Idle")
-		movement = Vector2.ZERO
 		
 	movement = move_and_slide(movement)
 	
@@ -40,4 +38,8 @@ func _on_Timer_timeout():
 
 
 func _on_Area2D_body_entered(body):
-	movement = Vector2.ZERO
+	if body != self:
+		movement = Vector2.ZERO
+	
+func _on_Area2D_body_exited(body):
+	movement = player.position - self.position
